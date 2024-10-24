566d1274444f19bcd3071ece29491d6c602c5772
KarlitosD <carlosdcampos.04@gmail.com>
2024-10-24 05:06:41 -0300

    chore: basic config of eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginTailwind from "eslint-plugin-tailwindcss";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx,astro}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginTailwind.configs["flat/recommended"],
+  {
+    ...pluginReact.configs.flat.recommended,
+    files: ["./src/**/*.{jsx}"],
+  },
+  {
+    rules : {
+      "no-var": "error",
+      "astro/no-exports-from-components": "error",
+      "no-unused-vars": "warn",
+    }
+  }
+];
