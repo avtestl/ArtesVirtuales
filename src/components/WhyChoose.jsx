5383d290a37397fb2402481c7a195f7be41e73e0
KarlitosD <carlosdcampos.04@gmail.com>
2024-12-02 05:45:39 -0300

    feat: create why choose component for home page

---
+++ src/components/WhyChoose.jsx
@@ -0,0 +1,49 @@
+import ReasonOneImg from "../assets/Reason1.png"
+import ReasonTwoImg from "../assets/Reason2.png"
+import ReasonThreeImg from "../assets/Reason3.png"
+
+function Reason({ img, title, body }){
+  return (
+      <div className="text-center">
+        <h3 className="text-xl font-medium text-[#2F2E41]">
+          {title}
+        </h3>
+        <div className="my-4 flex items-center justify-center">
+          {img}
+        </div>
+        <p className="text-lg text-[#2F2E41]">
+          {body}
+        </p>
+    </div>
+  )
+}
+
+export function WhyChooseUs () {
+  return (
+    <section className="bg-[#D1E0EB] px-6 py-12">
+      <h2 className="mb-16 text-center text-4xl font-semibold text-[#2F2E41]">
+        ¿Porqué Elegirnos?
+      </h2>
+
+      <div className="space-y-12 flex flex-col items-center gap-6">
+        <Reason
+          img={<img src={ReasonOneImg.src} alt="Why Choose Us" width={396} height={190} />}
+          title="Experiencia y Profesionalismo"
+          body="Contamos con un equipo de expertos en diversas áreas del marketing digital."
+        />
+
+        <Reason
+          img={<img src={ReasonTwoImg.src} alt="Why Choose Us" width={296} height={232} className="ml-1" />}
+          title="Resultados Comprobados"
+          body="Hemos ayudado a numerosas empresas a alcanzar sus metas y superar sus expectativas."
+        />
+
+        <Reason 
+          img={<img src={ReasonThreeImg.src} alt="Why Choose Us" width={300} height={250} className="ml-4" />}
+          title="Enfoque Personalizado"
+          body="Cada cliente es único, por lo que ofrecemos soluciones a medida que se adaptan a tus necesidades específicas."
+        />
+      </div>
+    </section>
+  );
+};
