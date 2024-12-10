47a6731d5d549837eccd790980f9d960a97b9747
trolerogame <danielgcampos.04@gmail.com>
2024-12-10 14:09:08 -0300

    add whatsappButton in page

---
+++ src/components/WhatsappButton.jsx
@@ -0,0 +1,11 @@
+
+
+
+const WhatsappBUtton = ({whatsapp}) => {
+  return <a href={whatsapp} target="_blank" className="fixed bottom-4 right-6" >
+    <img src="/whatsapp.svg" alt="whatsapp" width={50} height={50} />
+  </a>
+}
+
+
+export default WhatsappBUtton
