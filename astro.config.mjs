3045e4d87c6d94c8009aeaa28c25856075a7b4b6
KarlitosD <carlosdcampos.04@gmail.com>
2024-12-12 01:00:21 -0300

    feat: add plugins for optimize site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import playformInline from "@playform/inline";
+
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind(),
+    playformInline(),
+    playformCompress()
+  ]
 });
