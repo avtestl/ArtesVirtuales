3e3585ea83c8da8a8fde81d9acec9de2197b75d6
Maurico <abel772003@gmail.com>
2024-12-19 12:59:53 -0300

    probando sistema

--- src/components/equipo.jsx
+++ src/components/equipo.jsx
@@ -1,23 +1,11 @@
-import { useState, useEffect } from "react"; 
-import AnimatedImage from "./AnimatedImage";
+import { useState } from "react";
 import "../styles/equipo.css";
 
+
+
+
 const EquipoSection = ({ data }) => {
-  const [isDesktop, setIsDesktop] = useState(false); // Detecta si es escritorio
   const [expanded, setExpanded] = useState({});
-  const MAX_CHARACTERS = 150; // Número de caracteres visibles por defecto
-  // const { nombreCompleto, videoPresentacion, tituloPuesto, experiencia } = data;
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 1024);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   const toggleDescription = (index) => {
     setExpanded((prevState) => ({
@@ -26,7 +14,7 @@ const EquipoSection = ({ data }) => {
     }));
   };
 
-  const { nombreCompleto, videoPresentacion, tituloPuesto, experiencia, bannerImagen } = data;
+  const { nombreCompleto, videoPresentacion, tituloPuesto, experiencia } = data;
 
   return (
 
@@ -37,6 +25,7 @@ const EquipoSection = ({ data }) => {
           <h1>{nombreCompleto}</h1>
           <video
             src={videoPresentacion}
+            autoPlay
             poster={data.bannerImagen}
             controls
           >
@@ -48,61 +37,38 @@ const EquipoSection = ({ data }) => {
 
       <section className="proyectos">
         <h2>Trabajos/Proyectos</h2>
-        <div className="contenedor-trabajos">
-          {experiencia.map((proyecto, index) => {
-            const isExpanded = expanded[index] || isDesktop;
-            const shouldShowButton =
-              proyecto.descripcion.length > MAX_CHARACTERS && !isDesktop;
-
-            return (
-              <article key={index}>
-                <AnimatedImage
-                  src={proyecto.imagen}
-                  alt="imagen de trabajo realizado"
-                />
-                <h3>{proyecto.tituloOEmpresa}</h3>
-                <p className="fecha-trabajo">
-                  <strong>Inicio:</strong> {proyecto.inicio} <br />
-                  <strong>Final:</strong> {proyecto.final}
-                </p>
-                <h4>{proyecto.puesto}</h4>
-                <p className="project-description">
-                  {isExpanded
-                    ? proyecto.descripcion
-                    : `${proyecto.descripcion.substring(0, MAX_CHARACTERS)}...`}
-                </p>
-                <div className="cajaDeBotones">
-                  {shouldShowButton && (
-                    <button
-                      className="toggle-btn"
-                      onClick={() => toggleDescription(index)}
-                    >
-                      {isExpanded ? "Ver menos" : "Ver más"}
-                    </button>
-                  )}
-                  {proyecto.urlTrabajo && (
-                    <button>
-                      <a
-                        href={proyecto.urlTrabajo}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Ver Proyecto
-                      </a>
-                    </button>
-                  )}
-                </div>
-              </article>
-            );
-          })}
-        </div>
+        {experiencia.map((proyecto, index) => (
+          <article key={index}>
+            <img src={proyecto.imagen} alt="imagen de trabajo realizado" />
+            <h3>{proyecto.tituloOEmpresa}</h3>
+            <p
+              className={`project-description ${
+                expanded[index] ? "expanded" : ""
+              }`}
+            >
+              {proyecto.descripcion}
+              {expanded[index] && (
+                <span className="hidden-text">{proyecto.puesto}</span>
+              )}
+            </p>
+            <button
+              className="toggle-btn"
+              onClick={() => toggleDescription(index)}
+            >
+              {expanded[index] ? "Ver menos" : "Ver mas"}
+            </button>
+            <button>
+              <a
+                href={proyecto.urlTrabajo}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Ver Proyecto
+              </a>
+            </button>
+          </article>
+        ))}
       </section>
-
-      <div className="volver-inicio">
-        <button onClick={() => (window.location.href = "/")}>
-          Volver al Inicio
-        </button>
-      </div>
     </main>
   );
 };
