9c3a85f75230be5079e23b89fbf0af7a93135d24
trolerogame <danielgcampos.04@gmail.com>
2025-02-11 17:20:04 -0300

    add LinkedIn link to representative component and include new social media icons

--- src/components/Representative.jsx
+++ src/components/Representative.jsx
@@ -1,8 +1,8 @@
 
 export default function Representante({dataRepresentative}) {
   return (
-    <div className={`flex flex-col justify-center items-center my-10 relative bg-cover bg-[url(${dataRepresentative.fondo})] md:bg-[url(${dataRepresentative.fondoDesk})]`} >
-			<div className="bg-black absolute w-full h-full z-1 opacity-60" ></div>
+    <div className={`flex flex-col justify-center items-center my-10 relative bg-cover bg-[url(/wallpaperRepresentant.png)] md:bg-[url(/wallpaperRepresentantDesk.jpg)]`} >
+			<div className="bg-black absolute w-full h-full z-1 opacity-60"></div>
 			<div className="z-10 mb-10">
 				<h2 className="text-3xl font-semibold text-center text-white mb-6 mt-12" dangerouslySetInnerHTML={{__html: dataRepresentative.titulo}} ></h2>
 				<div className="grid md:grid-cols-2 gap-12 items-center px-6 md:px-10 mt-10" >
@@ -18,6 +18,11 @@ export default function Representante({dataRepresentative}) {
 						<p className="mt-2 text-white text-sm text-center md:text-start md:text-lg ">
 							{dataRepresentative.descripcion}
 						</p>
+						<div className="flex justify-center">
+							<a href={dataRepresentative.linkedin} target="_blank" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block mt-4">
+								LinkedIn
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
